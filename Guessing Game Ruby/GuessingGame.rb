4110edae0c26a878afb6d42b2d2deb3c0b30f261
sh999 <satrio.h@gmail.com>
2015-03-09 20:36:48 -0400

    Finish guessing game

--- Guessing Game Ruby/GuessingGame.rb
+++ Guessing Game Ruby/GuessingGame.rb
@@ -1,9 +1,23 @@
-guess = 80
+# Simple guessing game in Ruby
+puts "Guess my number = "
+guess = gets.chomp
+guess = guess.to_i
 correctNumber = 80
-if guess < correctNumber
-	puts "Guess is too low"
-elsif guess > correctNumber
-	puts "Guess is too high"
-elsif guess == correctNumber
-	puts "Correct!"
+puts "Your guess is #{guess}"
+correct = false
+while correct == false
+	if guess < correctNumber
+		puts "Guess is too low"
+	elsif guess > correctNumber
+		puts "Guess is too high"
+	elsif guess == correctNumber
+		puts "Correct!"
+		correct = true
+	end
+	
+	if correct == false
+		puts "Guess again "
+		guess = gets.chomp
+		guess = guess.to_i
+	end
 end
