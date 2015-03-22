8cf4c84625d22d651f44f86a39084301e95c6e3e
sh999 <satrio.h@gmail.com>
2015-03-22 13:43:23 -0400

    Working on WarriorSet and Simulate message passing

--- Battle Simulator/battlesim.py
+++ Battle Simulator/battlesim.py
@@ -5,45 +5,59 @@ Simulator mechanics can be as simple as dice-based or
 
 
 class Master():
+	'''
+	Master game class.  Currently its only job is to display message
+	May be expanded to includ e menu-related or big game logic behavior, not specific gameplay
+	'''
 	def __init__(self):
 		pass
+	
 	def play(self):
-		pass
+		self.introMessage()
+
+	def introMessage(self):
+		print "\n\n"
+		print "=================================="
+		print "|| Welcome to Battle Simulator! ||"
+		print "=================================="
+		print "\n"
+		print "You have 100 points to spend" # Implement later
 
 class Warrior():
 	def __init__(self, attack, defense, agility):
 		self.attack = attack
 		self.defense = defense
 		self.agility = agility
-
-def introMessage():
-	print "\n\n"
-	print "=================================="
-	print "|| Welcome to Battle Simulator! ||"
-	print "=================================="
-	print "\n"
-	print "You have 100 points to spend" # Implement later
 	
-class SetupPlayers():	# Getting too big.  Split with WarriorSet class
+class SetupPlayers():	
+	''' Getting too big.  Split with WarriorSet class.  
+		Purpose:  Set warrior attribute values by asking user and store the Warrior objects; can be retrieved by other classes, like WarriorSet
+	'''
 	def __init__(self):
 		pass
+	
 	def askPlayer(self):	
-		# self.askForAttributes()
-		self.setDefaultAttributes()
+		self.askForAttributes()
+		# self.setDefaultAttributes()
 		self.playerWarrior = Warrior(self.attack, self.defense, self.agility)
 		self.computerWarrior = Warrior(5, 3, 7)
+	
 	def askForAttributes(self):
 		self.attack = raw_input("Enter attack value: ")
 		self.defense = raw_input("Enter defense value: ")
 		self.agility = raw_input("Enter agility value: ")
+	
 	def setDefaultAttributes(self):
 		self.attack = 7
 		self.defense = 8
 		self.agility = 9
-	def getPlayerWarriorStats(self):
+
+	def getPlayerWarrior(self):
 		return self.playerWarrior
-	def getComputerWarriorStats(self):
+
+	def getComputerWarrior(self):
 		return self.computerWarrior
+
 	def displayStats(self):
 		print "\nYour warrior stats: "
 		print "Attack: ", self.playerWarrior.attack
@@ -55,25 +69,45 @@ class SetupPlayers():	# Getting too big.  Split with WarriorSet class
 		print "Defense: ", self.computerWarrior.defense
 		print "Agility: ", self.computerWarrior.agility
 		print "\n"
+
 	def run(self):
-		askPlayer()
-		displayStats()
+		self.askPlayer()
+		self.displayStats()
 
 class WarriorSet:
-	def __init__(self):
-		pass
+	'''
+	Holds player and computer warriors
+	'''
+	def __init__(self, warrior1, warrior2):
+		self.warrior1 = warrior1
+		self.warrior2 = warrior2
+
+	def printWarriors(self):
+		print self.warrior1
+		print self.warrior2
+
+class Simulation():
+	def __init__(self, warrior1, warrior2):
+		self.warrior1 = warrior1
+		self.warrior2 = warrior2
+
+	def run(self, warrior1, warrior2):
+		print warrior1
+		print warrior2
 
 def simulateBattle():
 	print "***** !Battle Time! *****"
 
 def main():
 	battlesimulator = Master()
-	battlesimulator.play()
+	battlesimulator.play()	# Currently displays only intro message
 
-	introMessage()
 	setupPlayers = SetupPlayers()
 	setupPlayers.run()
-	
-	simulateBattle()
+
+	warriorSet = WarriorSet(Warrior(1, 2, 3), Warrior(4, 5, 6))
+	warriorSet.printWarriors()
+	# simulation = Simulation()
+	# simulation.run()
 	
 main()
