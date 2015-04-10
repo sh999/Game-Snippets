f77e6b65883a4c26de05ae0d4e901915f10d6aa9
sh999 <satrio.h@gmail.com>
2015-04-10 17:59:59 -0400

    Draw pygame window

--- Evolution/evolution.py
+++ Evolution/evolution.py
@@ -4,6 +4,24 @@ that simulates terrain, creatures,
 ecology, evolution, and interaction between
 organisms
 '''
+import pygame, sys
+from pygame.locals import *
 
+def main():
+	pygame.init()
+	surface = pygame.display.set_mode((200, 200))
+	pygame.display.set_caption("Evolution")
+	green = (0,255,0)
+	surface.fill(green)
+	pygame.draw.ellipse(surface, (222,22,22), (22,22,33,33))
+	while True:
+		for event in pygame.event.get():
+			mouseIsPressed = pygame.mouse.get_pressed()[0]
+			if mouseIsPressed == True:
+				print "Mouse pressed"
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+		pygame.display.update()
 
-
+main()
